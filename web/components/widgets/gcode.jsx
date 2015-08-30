e06c509aa00c7e0a4ea325146f4f60f6c7a0c568
Cheton Wu <cheton@gmail.com>
2015-08-30 22:25:09 +0800

    dev

--- web/components/widgets/gcode.jsx
+++ web/components/widgets/gcode.jsx
@@ -1,9 +1,30 @@
 import i18n from 'i18next';
 import React from 'react';
+import ReactList from 'react-list';
 import Widget from '../widget';
 import './gcode.css';
 
 export default class GcodeWidget extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            commands: []
+        };
+    }
+    handleCommands() {
+        var fs = require('fs'); // FIXME
+        var file = fs.readFileSync(__dirname + '/../../../test/github.gcode', 'utf8');
+        var commands = file.split('\n');
+        this.setState({
+            commands: commands
+        });
+    }
+    renderItem(index, key) {
+        return <div className="list-item" key={key}>
+            <div className="number">{index + 1}</div>
+            <div className="command">{this.state.commands[index]}</div>
+        </div>;
+    }
     render() {
         var options = {
             width: 300,
@@ -24,6 +45,15 @@ export default class GcodeWidget extends React.Component {
             },
             content: (
                 <div data-component="Widgets/GcodeWidget">
+                    <button type="button" className="btn btn-default" name="btn-play" title="Play" onClick={this.handleCommands.bind(this)}>
+                        <i className="icon ion-play"></i>
+                    </button>
+                    <div style={{overflow: 'auto', maxHeight: 250}}>
+                        <ReactList
+                            itemRenderer={this.renderItem.bind(this)}
+                            length={this.state.commands.length}
+                        />
+                    </div>
                 </div>
             )
         };

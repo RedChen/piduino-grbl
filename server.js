281a567604c322093bef03864ba8a4f3955b196c
Cheton Wu <cheton@gmail.com>
2015-09-02 23:28:03 +0800

    dev

--- server.js
+++ server.js
@@ -114,126 +114,125 @@ module.exports = function(server) {
 
     });
 
+    /*
+    socket.on('serialport:connect', function(data) {
+        var path = data.port;
+        var baudrate = Number(data.baudrate);
+        var queue = motionQueue();
 
-        /*
-        socket.on('serialport:connect', function(data) {
-            var path = data.port;
-            var baudrate = Number(data.baudrate);
-            var queue = motionQueue();
-
-            if ( ! sp) {
-                sp = new serialport.SerialPort(path, {
-                    baudrate: baudrate, // defaults to 9600
-                    parser: serialport.parsers.readline('\n')
-                });
-            }
-
-            log.info('Starting a new serial port connection: sockets=%d', sockets.length);
-
-            queue.on('data', function(line) {
-                line = line.trim();
-                console.log(line);
-                sp.write(line + '\n');
-                socket.emit('serialport:data', line);
+        if ( ! sp) {
+            sp = new serialport.SerialPort(path, {
+                baudrate: baudrate, // defaults to 9600
+                parser: serialport.parsers.readline('\n')
             });
+        }
 
-            sp.on('open', function() {
-                log.debug('Connected to \'%s\' at %d.', path, baudrate);
-                socket.emit('serialport:open', {
-                    port: path,
-                    baudrate: baudrate
-                });
+        log.info('Starting a new serial port connection: sockets=%d', sockets.length);
+
+        queue.on('data', function(line) {
+            line = line.trim();
+            console.log(line);
+            sp.write(line + '\n');
+            socket.emit('serialport:data', line);
+        });
+
+        sp.on('open', function() {
+            log.debug('Connected to \'%s\' at %d.', path, baudrate);
+            socket.emit('serialport:open', {
+                port: path,
+                baudrate: baudrate
             });
+        });
 
-            sp.on('data', function(line){
-                line = line.trim();
+        sp.on('data', function(line){
+            line = line.trim();
 
-                console.log('grbl>', line);
+            console.log('grbl>', line);
 
-                if (line === 'ok') {
-                    queue.next();
-                } else if (line === 'error') {
-                    queue.next();
-                } else if (/<[^>]+>/.test(line)){
-                    // <Idle,MPos:0.000,0.000,0.000,WPos:0.000,0.000,0.000>
-                    var r = line.match(/<(\w+),\w+:([^,]+),([^,]+),([^,]+),\w+:([^,]+),([^,]+),([^,]+)>/);
-                    if ( ! r) {
-                        return;
-                    }
-
-                    // https://github.com/grbl/grbl/wiki/Configuring-Grbl-v0.9#---current-status
-                    socket.emit('grbl:status', {
-                        activeState: r[1], // Active States: Idle, Run, Hold, Door, Home, Alarm, Check
-                        machinePos: { // Machine position
-                            x: r[2], 
-                            y: r[3],
-                            z: r[4]
-                        },
-                        workingPos: { // Working position
-                            x: r[5],
-                            y: r[6],
-                            z: r[7]
-                        }
-                    });
-                } else {
-                    // Others
+            if (line === 'ok') {
+                queue.next();
+            } else if (line === 'error') {
+                queue.next();
+            } else if (/<[^>]+>/.test(line)){
+                // <Idle,MPos:0.000,0.000,0.000,WPos:0.000,0.000,0.000>
+                var r = line.match(/<(\w+),\w+:([^,]+),([^,]+),([^,]+),\w+:([^,]+),([^,]+),([^,]+)>/);
+                if ( ! r) {
+                    return;
                 }
 
-                socket.emit('serialport:data', 'grbl> ' + line);
-            });
+                // https://github.com/grbl/grbl/wiki/Configuring-Grbl-v0.9#---current-status
+                socket.emit('grbl:status', {
+                    activeState: r[1], // Active States: Idle, Run, Hold, Door, Home, Alarm, Check
+                    machinePos: { // Machine position
+                        x: r[2], 
+                        y: r[3],
+                        z: r[4]
+                    },
+                    workingPos: { // Working position
+                        x: r[5],
+                        y: r[6],
+                        z: r[7]
+                    }
+                });
+            } else {
+                // Others
+            }
 
-            sp.on('close', function() {
-                log.debug('The serial port connection is closed.');
-            });
-
-            sp.on('error', function() {
-                log.error('Error opening serial port \'%s\'', path);
-            });
-
-            socket.on('serialport:write', function(data) {
-                log.debug('serialport:write: data=%s', data);
-                sp.write(data);
-            });
-
-            socket.on('serialport:writeln', function(line) {
-                log.debug('serialport:writeln: line=%s', line);
-                sp.write(line + '\n');
-            });
-
-            socket.on('grbl:start', function() {
-                var lines = [];
-
-                readline
-                    .createInterface({
-                        input: fs.createReadStream('./test/github.gcode'),
-                        // No output
-                        terminal: false
-                    })
-                    .on('line', function(line) {
-                        line = stripComments(line);
-                        if (line.length === 0) {
-                            return;
-                        }
-                        lines.push(line);
-                    })
-                    .on('close', function() {
-                        queue.add(lines);
-                        queue.start();
-                    });
-            });
-
-            socket.on('grbl:pause', function() {
-                queue.pause();
-            });
-
-            socket.on('grbl:resume', function() {
-                queue.resume();
-            });
-
-            socket.on('grbl:reset', function() {
-                queue.reset();
-            });
-
+            socket.emit('serialport:data', 'grbl> ' + line);
         });
-        */
+
+        sp.on('close', function() {
+            log.debug('The serial port connection is closed.');
+        });
+
+        sp.on('error', function() {
+            log.error('Error opening serial port \'%s\'', path);
+        });
+
+        socket.on('serialport:write', function(data) {
+            log.debug('serialport:write: data=%s', data);
+            sp.write(data);
+        });
+
+        socket.on('serialport:writeln', function(line) {
+            log.debug('serialport:writeln: line=%s', line);
+            sp.write(line + '\n');
+        });
+
+        socket.on('grbl:start', function() {
+            var lines = [];
+
+            readline
+                .createInterface({
+                    input: fs.createReadStream('./test/github.gcode'),
+                    // No output
+                    terminal: false
+                })
+                .on('line', function(line) {
+                    line = stripComments(line);
+                    if (line.length === 0) {
+                        return;
+                    }
+                    lines.push(line);
+                })
+                .on('close', function() {
+                    queue.add(lines);
+                    queue.start();
+                });
+        });
+
+        socket.on('grbl:pause', function() {
+            queue.pause();
+        });
+
+        socket.on('grbl:resume', function() {
+            queue.resume();
+        });
+
+        socket.on('grbl:reset', function() {
+            queue.reset();
+        });
+
+    });
+    */
 };
